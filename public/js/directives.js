cf7ef82e40f6eeaf15d2c0846ecc5b837c13909b
Sandeep Panda <sandeep@thexprs.com>
2014-10-16 23:02:47 +0530

    Initial Commit

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,4 +1,4 @@
-angular.module('com.htmlxprs.autocomplete.directives',[]).directive('autoComplete',['$http','$timeout',function($http,$timeout){
+angular.module('com.htmlxprs.autocomplete.directives',[]).directive('autoComplete',['$http',function($http){
     return {
         restrict:'AE',
         scope:{
@@ -11,14 +11,7 @@ angular.module('com.htmlxprs.autocomplete.directives',[]).directive('autoComplet
 
             scope.selectedTags=[];
 
-            scope.shiftFocus=function(event){
-                if(event.keyCode===40) {
-                    $timeout(function(){
-                        elem.find('#suggestions').focus();
-                        scope.selectedIndex=0;
-                    });
-                }
-            }
+            scope.selectedIndex=-1;
 
             scope.removeTag=function(index){
                 scope.selectedTags.splice(index,1);
@@ -30,6 +23,7 @@ angular.module('com.htmlxprs.autocomplete.directives',[]).directive('autoComplet
                         data.unshift(scope.searchText);
                     }
                     scope.suggestions=data;
+                    scope.selectedIndex=-1;
                 });
             }
 
@@ -37,30 +31,33 @@ angular.module('com.htmlxprs.autocomplete.directives',[]).directive('autoComplet
                 if(scope.selectedTags.indexOf(scope.suggestions[index])===-1){
                     scope.selectedTags.push(scope.suggestions[index]);
                     scope.searchText='';
-                    scope.selectedIndex=-1;
                     scope.suggestions=[];
-                    elem.find('#searchInput').focus();
                 }
             }
 
             scope.checkKeyDown=function(event){
-                //arrow down
                 if(event.keyCode===40){
+                    event.preventDefault();
                     if(scope.selectedIndex+1 !== scope.suggestions.length){
                         scope.selectedIndex++;
                     }
                 }
-                //arrow up
                 else if(event.keyCode===38){
+                    event.preventDefault();
                     if(scope.selectedIndex-1 !== -1){
                         scope.selectedIndex--;
                     }
                 }
-                //enter
                 else if(event.keyCode===13){
                     scope.addToSelectedTags(scope.selectedIndex);
                 }
             }
+
+            scope.$watch('selectedIndex',function(val){
+                if(val!==-1) {
+                    scope.searchText = scope.suggestions[scope.selectedIndex];
+                }
+            });
         }
     }
 }]);
